5de39b7391e639f271ccd605d7663e90ec2d5de0
Woojun Lee <zoaal22@gmail.com>
2022-12-10 17:35:41 +0900

    Initial Commit

---
+++ IntroToGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroToGit
+//
+//  Created by Woojun Lee on 2022/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
